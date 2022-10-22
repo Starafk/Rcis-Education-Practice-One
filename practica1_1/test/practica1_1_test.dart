e28a7c0ffdf498513125180f1322e4ebb82584b3
Starafk <rfkeu20@gmail.com>
2022-10-22 18:24:07 +0700

    First commit. Create console app

--- practica1_1/test/practica1_1_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:practica1_1/practica1_1.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
--- practica_1_1/test/practica_1_1_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:practica_1_1/practica_1_1.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('List.indexOf', () {
-    expect(int List.indexOf (E element, [int start = 0])(), );
-  });
-}
